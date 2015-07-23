59aca73fd59fd3c3662afc62374f64a0eb9b225c
FrankKangJun <244652411@qq.com>
2015-07-23 13:59:07 +0800

    增加seller的登录。查找用户数据，检查用户是否注册的接口 修改了getHomefood接口

---
+++ src/main/java/com/changyu/foryou/mapper/SellerMapper.java
@@ -0,0 +1,17 @@
+package com.changyu.foryou.mapper;
+
+import java.util.Date;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.changyu.foryou.model.Sellers;
+
+public interface SellerMapper {
+
+	int test();
+
+	Sellers selectByCampusAdmin(String campusAdmin);
+
+	void updateLastLoginTime(@Param(value="date")Date date, @Param(value="campusAdmin") String campusAdmin);
+
+}
---
+++ src/main/java/com/changyu/foryou/service/SellerService.java
@@ -0,0 +1,14 @@
+package com.changyu.foryou.service;
+
+import java.util.Date;
+
+import com.changyu.foryou.model.Sellers;
+
+
+public interface SellerService {
+	public int test();
+
+	public Sellers selectByCampusAdmin(String campusAdmin);
+
+	public void updateLastLoginTime(Date date, String campusAdmin);
+}
