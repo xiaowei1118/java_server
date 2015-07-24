812617b96b04b1a01d1d65d4dd09829c56f079da
SimonSunDev <simon.sun.dev@gmail.com>
2015-07-24 15:45:37 +0800

    update web

---
+++ src/main/webapp/js/util.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+// 返回cookies字符串中指定键对应的值
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name) == 0)
+			return c.substring(name.length, c.length);
+	}
+	return "";
+}
