1a60e4c2ce253674433e8c17076e92cc56b22180
FrankKangJun <244652411@qq.com>
2015-07-29 15:49:17 +0800

    f改为F
    11

--- src/main/java/com/changyu/foryou/tools/Constants.java
+++ src/main/java/com/changyu/foryou/tools/Constants.java
@@ -5,5 +5,5 @@ public  class Constants {
 	final static public String SUCCESS = "success";
 	final static public String FAILURE = "failure";
 	public static final String MESSAGE = "message";
-	public static final String localIp = "http://120.26.76.252:8080/foryouImage";
+	public static final String localIp = "http://120.26.76.252:8080/ForyouImage";
 }
